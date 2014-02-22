a7eb27eaf3c45609e85604c1933805c1e5042b45
Kevin Montuori <montuori@gmail.com>
2014-02-21 21:31:35 -0600

    access key ok; small test refactor

--- lib/AWS/Signature2.ex
+++ lib/AWS/Signature2.ex
@@ -1,5 +1,6 @@
 defmodule AWS.Signature2 do
 
   def secret_key, do: System.get_env("AWS_SECRET_KEY")
+  def access_key, do: System.get_env("AWS_ACCESS_KEY")
 
 end
--- test/ex_aws_signature_2_test.exs
+++ test/ex_aws_signature_2_test.exs
@@ -1,9 +1,10 @@
 defmodule ExAwsSignature_2Test do
   use ExUnit.Case
 
+  example_access_key = "AKIAIOSFODNN7EXAMPLE"
   example_secret_key = "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
   _example_query = "https://elasticmapreduce.amazonaws.com?" <>
-                  "AWSAccessKeyId=AKIAIOSFODNN7EXAMPLE&" <>
+                  "AWSAccessKeyId=#{example_access_key}&" <>
                   "Action=DescribeJobFlows&" <>
                   "SignatureMethod=HmacSHA256&" <>
                   "SignatureVersion=2&" <>
@@ -12,10 +13,18 @@ defmodule ExAwsSignature_2Test do
                   "Signature=i91nKc4PWAt0JJIdXwz9HxZCJDdiy6cf%2FMj6vPxyYIs%3D"
   
   System.put_env("AWS_SECRET_KEY", example_secret_key)
-
+  System.put_env("AWS_ACCESS_KEY", example_access_key)
 
   test "found secret key" do
-    assert AWS.Signature2.secret_key ==
-                            "wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY"
+    assert AWS.Signature2.secret_key
+    assert AWS.Signature2.secret_key == System.get_env("AWS_SECRET_KEY")
   end
+
+  test "found access key" do
+    assert AWS.Signature2.access_key
+    assert AWS.Signature2.access_key == System.get_env("AWS_ACCESS_KEY")
+  end
+
+
+
 end
